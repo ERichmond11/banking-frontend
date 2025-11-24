d25755d9c9b8fb81e1e2f23051196483f4e5d85e
Emmanuel Richmond <71474351+ERichmond11@users.noreply.github.com>
2025-11-24 02:06:41 -0500

    Update TransferCenter.jsx

--- src/pages/TransferCenter.jsx
+++ src/pages/TransferCenter.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { useNavigate, Link } from "react-router-dom";
+import api from "../api/axiosClient";
 import NavBar from "../components/NavBar";
 
 function TransferCenter() {
